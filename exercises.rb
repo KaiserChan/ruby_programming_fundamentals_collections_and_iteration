78ac8cfe9e394602999ef477f03fc29974c5f9c6
KaiserChan <chankaiserkk@gmail.com>
2017-08-10 15:08:32 -0400

    Created exercises.rb - Completed exercise 0

---
+++ exercises.rb
@@ -0,0 +1,36 @@
+#  EXERCISE 0 BELOW ----------
+
+# Arrays here
+fav_colors = ["white", "black", "blue", "yellow", "grey"]
+my_age = [35, 35, 38, 36, 23]
+coin_flip = ["head", "head", "tail", "head", "tail"]
+artist = ["Eason Chan", "Maroon 5", "John Legend"]
+colors_symbol = [:white, :black, :blue, :yellow, :grey]
+
+# Hashes here
+my_words = {
+  swole: "An unmatchable awesomeness",
+  aww: "The sound you make when you see something adorable",
+  hangry: "Getting angry because of hunger"
+}
+
+my_movies = {
+  lotr: 2006,
+  avengers: 2010,
+  no_pain_no_gain: 2012,
+  san_andreas: 2015,
+}
+
+my_cities = {
+  hongkong: 7000000,
+  toronto: 2000000,
+  berlin: 1000000
+}
+
+my_siblings = {
+  leo: 38,
+  may: 36,
+  jero: 23,
+  ken: 32,
+  sebastian: 35
+}
