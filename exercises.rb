77f803475ee5b124470e373d0657b5db154843a7
KaiserChan <chankaiserkk@gmail.com>
2017-08-10 21:25:20 -0400

    Updated exercises.rb - Completed exercise 6

--- exercises.rb
+++ exercises.rb
@@ -39,30 +39,40 @@ my_siblings = {
 
 #  EXERCISE 1 BELOW ----------
 
+#1
 # puts coin_flip
 
+#2
 # puts fav_colors[0]
 
+#3
 # puts my_age.sort
 
+#4
 # puts my_age.insert(0,0)
 
+#5
 # puts my_movies[:san_andreas]
 
 
 
 #  EXERCISE 2 BELOW ----------
 
+#1
 # puts fav_colors.last
 
+#2
 # my_cities[:los_angeles] = 10000000
 # puts my_cities
 
+#3
 # coin_flip = coin_flip.reverse
 # puts coin_flip
 
+#4
 # puts my_cities[:hongkong]
 
+#5
 # artist.each do |item|
 #   puts "I think #{item} is super awesome!"
 # end
@@ -71,16 +81,19 @@ my_siblings = {
 
 #  EXERCISE 3 BELOW ----------
 
-# puts artist[0]
-# puts artist[1]
+#1
+# puts artist[0..1]
 
+#2
 # my_movies.each do |name, year|
 #   puts "#{name} came out in #{year}."
 # end
 
+#3
 # my_age = my_age.sort.reverse
 # puts my_age
 
+#4
 # my_movies[:beauty_and_the_beast] = [1991, 2017]
 # my_movies.each do |name, year|
 #   puts "#{name} came out in #{year}."
@@ -90,23 +103,28 @@ my_siblings = {
 
 #  EXERCISE 4 BELOW ----------
 
+#1
 # my_age.each do |item|
 #   if item < 30
 #     puts item
 #   end
 # end
 
+#2
 # puts my_age.max
 
+#3
 # counts = Hash.new 0
 # coin_flip.each do |item|
 #   counts[item] += 1
 # end
 # puts counts
 
+#4
 # artist.delete("John Legend")
 # puts artist
 
+#5
 # my_cities[:toronto] = 3000000
 # puts my_cities
 
@@ -114,8 +132,10 @@ my_siblings = {
 
 #  EXERCISE 5 BELOW ----------
 
+#1
 # puts my_cities.values.sum
 
+#2
 # my_siblings.each do |name, age|
 #   if age >= 35
 #     puts "#{name} is old ..."
@@ -124,11 +144,73 @@ my_siblings = {
 #   end
 # end
 
+#3
 # puts fav_colors.last(2)
 
+#4
 # my_age.each do |age|
 #   puts age + 1
 # end
 
+#5
 # fav_colors += ["brown", "orange"]
 # puts fav_colors
+
+
+
+# EXERCISE 6 BELOW ----------
+
+#1
+# new_movies = {
+#   "1999": ["The Matrix", "Star Wars: Episode 1", "The Mummy"],
+#   "2009": ["Avatar", "Star Trek", "District 9"],
+#   "2019": ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]
+# }
+
+#2
+# phone_row = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ["*", 0, "#"]]
+# puts phone_row
+
+#3
+# new_countries = [
+#   {name: "korea", continent: "asia", island: false},
+#   {name: "germany", continent: "europe", island: false},
+#   {name: "australia", continent: "australia", island: true}
+# ]
+
+
+
+# EXERCISE 6 BELOW ----------
+#1
+# 20.times do puts "I will not skateboard in the halls" end
+
+#2
+# skateboard = [20.times do puts "I will not skateboard in the halls" end]
+# puts skateboard
+
+#3
+# one_to_fifty = [*1..50]
+# puts one_to_fifty
+
+#4
+# sum = 0
+# one_to_fifty.each do |add|
+#   sum += add
+# end
+# puts sum
+
+#5
+# one_to_fifty_triple = []
+# one_to_fifty.each do |number|
+#   3.times do
+#     one_to_fifty_triple.push(number)
+#   end
+# end
+# puts one_to_fifty_triple
+
+#6
+# not_island =
+# new_countries.select do |check|
+#   check[:island] == false
+# end
+# puts not_island
